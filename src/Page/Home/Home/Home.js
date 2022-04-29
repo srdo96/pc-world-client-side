1f112b02f0fc4c60d025fd5aadf0af7a0b27697c
Sakib Rahman <seu.sakib@gmail.com>
2022-04-29 20:50:00 +0600

    add banner

--- src/Page/Home/Home/Home.js
+++ src/Page/Home/Home/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import banner from "../../../img/cover/banner2.jpg";
 const Home = () => {
   return (
-    <div>
-      <h1>This is HOME</h1>
+    <div className="relative ">
+      <img className="rounded shadow-xl" src={banner} alt="" />
+
+      <div
+        className={`absolute bottom-10 py-5 text-xl  rounded-b-md  w-full text-white font-normal`}
+      >
+        <div className="text-center">
+          <h1 className="text-7xl ">
+            PC For Home and Office <br /> PC WORLD
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
