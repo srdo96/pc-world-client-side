4b8b0c57866a88e23d150aca0d5b6de103e4166c
Sakib Rahman <seu.sakib@gmail.com>
2022-04-29 15:35:59 +0600

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Page/Shared/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
