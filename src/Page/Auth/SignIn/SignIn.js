a7b40d8e7ccf097544db97b94157e8bbb3e081a5
Sakib Rahman <seu.sakib@gmail.com>
2022-05-01 01:29:55 +0600

    add google signin in signIn

--- src/Page/Auth/SignIn/SignIn.js
+++ src/Page/Auth/SignIn/SignIn.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 import loginCover from "../../../img/cover/loginCover.jpg";
+import Loading from "../../Shared/Loading/Loading";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+
+  if (googleUser) {
+    navigate("/");
+  }
   return (
     <div>
       <div className="flex flex-wrap w-full">
@@ -16,22 +26,32 @@ const SignIn = () => {
               <div className="flex gap-4 item-center">
                 {/* Google button */}
                 <button
+                  onClick={() => signInWithGoogle()}
                   type="button"
                   className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="mr-2"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
-                  </svg>
+                  {googleLoading ? (
+                    <Loading />
+                  ) : (
+                    <svg
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="mr-2"
+                      viewBox="0 0 1792 1792"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
+                    </svg>
+                  )}
                   Google
                 </button>
               </div>
+              {googleError && (
+                <p className="text-red-500 mt-4 text-center">
+                  {googleError.message}
+                </p>
+              )}
 
               <div className="flex mt-7 items-center text-center">
                 <hr className="border-gray-300 border-1 w-full rounded-md" />
